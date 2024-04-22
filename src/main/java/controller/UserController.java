88262aa848a1d25e8f75d58d68e4fe4ee3c2aae9
moheie <109584504+moheie@users.noreply.github.com>
2024-04-22 20:36:16 +0200

    added user model,service,controller

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class UserController {
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import model.User;
+import service.UserService;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserController {
+    @EJB
+    private UserService userService;
+
+    @POST
+    public Response createUser(User user) {
+        userService.createUser(user);
+        return Response.status(Response.Status.CREATED).entity(user).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getUserById(@PathParam("id") Long userId) {
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            return Response.ok(user).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateUser(@PathParam("id") Long userId, User updatedUser) {
+        updatedUser.setId(userId);
+        userService.updateUser(updatedUser);
+        return Response.ok(updatedUser).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteUser(@PathParam("id") Long userId) {
+        userService.deleteUser(userId);
+        return Response.noContent().build();
+    }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,34 @@
 package service;
 
-public class UserService {
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import model.User;
+
+@Stateless
+public class UserService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void createUser(User user) {
+        entityManager.persist(user);
+    }
+
+    public User getUserById(Long userId) {
+        return entityManager.find(User.class, userId);
+    }
+
+    public void updateUser(User updatedUser) {
+        entityManager.merge(updatedUser);
+    }
+
+    public void deleteUser(Long userId) {
+        User user = entityManager.find(User.class, userId);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+    
+    
 }
