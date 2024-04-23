ae4be93eb3b8736ba2e294d57c8df5baaf5bf0bb
moheie <109584504+moheie@users.noreply.github.com>
2024-04-23 21:56:02 +0200

    fixed 2.0

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,95 +1,22 @@
 package model;
 
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Card {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long cardId;
-	
-	private String name;
-	
-	private long assigendToId;
-	
-	//@ManyToOne
-	//@JoinColumn(name="listId")
-	
-	private ListofCards listOfCards;
-	
-	private String description;	
-	
-	private List<String> comments;
+	long id;
 
-	public Card() {
-		
-	}
-	
-	public Card(String name, long assigendToId, String description, List<String> comments) {
-		this.name = name;
-		this.assigendToId = assigendToId;
-		this.description = description;
-		this.comments = comments;
-	}
-	
+	String name;
 
-	public long getCardId() {
-		return cardId;
-	}
+	String description;	
 
-	public void setCardId(long cardId) {
-		this.cardId = cardId;
-	}
+	//List<String> comments;
 
-	public ListofCards getListOfCards() {
-		return listOfCards;
-	}
-
-	public void setListOfCards(ListofCards listOfCards) {
-		this.listOfCards = listOfCards;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public long getAssigendToId() {
-		return assigendToId;
-	}
-
-	public void setAssigendToId(long assigendToId) {
-		this.assigendToId = assigendToId;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public List<String> getComments() {
-		return comments;
-	}
-
-	public void setComments(List<String> comments) {
-		this.comments = comments;
-	}
-		
-	
-	
-	
 }
--- src/main/java/model/ListofCards.java
+++ src/main/java/model/ListofCards.java
@@ -1,60 +1,23 @@
 package model;
 
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 
 @Entity
 public class ListofCards{
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long listId;
-	
+	private long id;
+
 	private String name;
-	
-	//@OneToMany(mappedBy = "listOfCards")
-	
-	private List<Card> cards;
-	
-	public ListofCards() {
 
-	}
-	
-	public ListofCards(String name, List<Card> cards) {
-		this.name = name;
-		this.cards = cards;
-	}
+	//List<Cards> cards;
 
-	public long getListId() {
-		return listId;
-	}
 
-	public void setListId(long listId) {
-		this.listId = listId;
-	}
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Card> getCards() {
-		return cards;
-	}
-
-	public void setCards(List<Card> cards) {
-		this.cards = cards;
-	}
-	
-
-	
 }
