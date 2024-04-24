134a9d6c52195353a91ee806da9670366b09a41c
Ahmed Hagag <hagag1725@gmial.com>
2024-04-24 13:19:28 +0200

    Card Services APIs

--- src/main/java/controller/CardController.java
+++ src/main/java/controller/CardController.java
@@ -1,5 +1,69 @@
 package controller;
 
-public class CardController {
+import java.util.List;
 
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import model.Card;
+import service.CardService;
+
+@Path("/card")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class CardController {
+	
+	@EJB
+	private CardService cardService;
+	
+	@POST
+	@Path("/create")
+	public Response createCard(Card card, Long userId) {
+		String response = cardService.createCard(card, userId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@POST
+	@Path("/addComment")
+	public Response addCommentToCard(Long cardId, String comment) {
+		String response = cardService.addComment(cardId, comment);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@POST
+	@Path("/addDescription")
+	public Response addDescriptionToCard(Long cardId, String description) {
+		String response = cardService.addDescription(cardId, description);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@PUT
+	@Path("/move")
+	public Response moveCard(Long cardId, Long listId) {
+		String response = cardService.moveCard(cardId, listId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@DELETE
+	@Path("/delete")
+	public Response deleteCard(Long cardId) {
+		String response = cardService.deleteCard(cardId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@GET
+	@Path("/get")
+	public Response getCard(Long cardId) {
+		Card card = cardService.getCard(cardId);
+		return Response.status(Response.Status.CREATED).entity(card).build();
+	}
+	
+	@GET
+	@Path("/getComments")
+	public Response getComments(Long cardId) {
+		List<String> response = cardService.getComments(cardId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
 }
--- src/main/java/service/CardService.java
+++ src/main/java/service/CardService.java
@@ -1,5 +1,118 @@
 package service;
 
-public class CardService {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.Board;
+import model.Card;
+import model.ListofCards;
+
+@Stateless
+public class CardService {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	// create a new card.
+	public String createCard(Card card, Long userId) {
+		Card checkCard = entityManager.find(Card.class, card.getCardId());
+		ListofCards list = entityManager.find(ListofCards.class, card.getListofcards().getListId());
+		Board board = entityManager.find(Board.class, list.getBoard().getBoardId());
+		Boolean isMember = false;
+		for (Long memberId : board.getMembersIds()) {
+			if (memberId == userId) {
+				isMember = true;
+				break;
+			}
+		}
+		if (checkCard != null) {
+			return "Card already exists";
+		}
+		if (!isMember) {
+			return "User is not a member of the board";
+		}
+		entityManager.persist(card);
+		return "Card created successfully";
+		
+	}
+	
+	// move cards between lists.
+	public String moveCard(Long cardId, Long listId) {
+		Card card = entityManager.find(Card.class, cardId);
+		ListofCards list = entityManager.find(ListofCards.class, listId);
+		if (card != null && list != null) {
+			card.setListofcards(list);
+			return "Card moved successfully";
+		}
+		return "Card or list not found";
+	}
+	
+	// assign cards to themselves or other collaborators.
+	public String assignCard(Long cardId, Long userId) {
+		Card card = entityManager.find(Card.class, cardId);
+		if (card != null) {
+			card.setAssigendToId(userId);
+			return "Card assigned successfully";
+		}
+		return "Card not found";
+	}
+
+	// add descriptions to cards.
+	
+	public String addDescription(Long cardId, String description) {
+		Card card = entityManager.find(Card.class, cardId);
+		if (card != null) {
+			card.setDescription(description);
+			return "Description added successfully";
+		}
+		return "Card not found";
+	}
+	
+	// add comments to cards.
+	public String addComment(Long cardId, String comment) {
+        Card card = entityManager.find(Card.class, cardId);
+		if (card != null) {
+			card.getComments().add(comment);
+			return "Comment added successfully";
+		}
+		return "Card not found";
+	}
+	
+	// get all comments of a card.
+	public List<String> getComments(Long cardId) {
+        Card card = entityManager.find(Card.class, cardId);
+		if (card != null) {
+			return card.getComments();
+		}
+		return new ArrayList<>();
+	}
+	
+	//get card details.
+	public Card getCard(Long cardId) {
+		Card card = entityManager.find(Card.class, cardId);
+		return card;
+	}
+	
+	// delete a card.
+	public String deleteCard(Long cardId) {
+		Card card = entityManager.find(Card.class, cardId);
+		ListofCards list = entityManager.find(ListofCards.class, card.getListofcards().getListId());
+		Board board = entityManager.find(Board.class, list.getBoard().getBoardId());
+		Boolean isTeamLeader = false;
+		if (board.getTeamLeaderId() == card.getAssigendToId()) {
+			isTeamLeader = true;
+		}
+		
+		if (card != null && isTeamLeader) {
+			entityManager.remove(card);
+			return "Card deleted successfully";
+		}
+		if (isTeamLeader) {
+			return "insufficient permissions";
+		}
+		return "Card not found";
+	}
 }
