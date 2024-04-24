1ddff01c01e16874a57383b6e2efbffb25070685
Ahmed Hagag <hagag1725@gmial.com>
2024-04-24 12:00:56 +0200

    temp List

--- src/main/java/controller/ListController.java
+++ src/main/java/controller/ListController.java
@@ -1,5 +1,67 @@
 package controller;
 
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import model.Card;
+import model.ListofCards;
+import service.ListService;
+
+@Path("/list")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ListController {
+	@EJB
+	private ListService listService;
+	
+	@POST
+	@Path("/create")
+	public Response createList(ListofCards list) {
+		String response = listService.createList(list);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@POST
+	@Path("/addCard")
+	public Response addCardToList(Long listId, Long cardId) {
+		String response = listService.addCardToList(listId, cardId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	
+	@PUT
+	@Path("/update")
+	public Response updateList(ListofCards list) {
+		String response = listService.updateList(list);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@DELETE
+	@Path("/delete")
+	public Response deleteList(Long listId) {
+		String response = listService.deleteList(listId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	@DELETE
+	@Path("/removeCard")
+	public Response removeCardFromList(Long listId, Long cardId) {
+		String response = listService.removeCardFromList(listId, cardId);
+		return Response.status(Response.Status.CREATED).entity(response).build();
+	}
+	
+	@GET
+	@Path("/get/{id}")
+	public ListofCards getList(@PathParam("id") Long id) {
+		return listService.getList(id);
+	}
+	
+	@GET
+	@Path("/getCards/{id}")
+	public Set<Card> getCards(@PathParam("id") Long id) {
+		return listService.getCards(id);
+	}
 
 }
--- src/main/java/service/ListService.java
+++ src/main/java/service/ListService.java
@@ -1,5 +1,77 @@
 package service;
 
+import java.util.Set;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.Card;
+import model.ListofCards;
+
+@Stateless
 public class ListService {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public String createList(ListofCards list) {
+		ListofCards checkList = entityManager.find(ListofCards.class, list.getListId());
+		if (checkList != null) {
+			return "List already exists";
+		}
+		entityManager.persist(list);
+		return "List created successfully";
+	}
+	
+	public String updateList(ListofCards updatedList) {
+		ListofCards list = entityManager.find(ListofCards.class, updatedList.getListId());
+		if (list != null) {
+			entityManager.merge(updatedList);
+			return "List updated successfully";
+		}
+		return "List not found";
+	}
+	
+	public String deleteList(Long listId) {
+		ListofCards list = entityManager.find(ListofCards.class, listId);
+		if (list != null) {
+			entityManager.remove(list);
+			return "List deleted successfully";
+		}
+		return "List not found";
+	}
+	
+	public String addCardToList(Long listId, Long cardId) {
+		ListofCards list = entityManager.find(ListofCards.class, listId);
+		Card card = entityManager.find(Card.class, cardId);
+		if (list != null) {
+			list.getCards().add(card);
+			return "Card added to list successfully";
+		}
+		return "List not found";
+	}
+	
+	public String removeCardFromList(Long listId, Long cardId) {
+		ListofCards list = entityManager.find(ListofCards.class, listId);
+		Card card = entityManager.find(Card.class, cardId);
+		if (list != null) {
+			list.getCards().remove(card);
+			return "Card removed from list successfully";
+		}
+		return "List not found";
+	}
+	
+	
+	public ListofCards getList(Long listId) {
+		ListofCards list = entityManager.find(ListofCards.class, listId);
+		return list;
+	}
+	
+	
+	public Set<Card> getCards(Long listId) {
+		ListofCards list = entityManager.find(ListofCards.class, listId);
+		return list.getCards();
+	}
+	
 
 }
